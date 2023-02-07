183ec6fe09e26e531489beea957497a88f34c72d
Kadin Caldwell <kcwell000@gmail.com>
2023-02-06 18:02:39 -0600

    completed lab

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(spy) {
+    return (spy())
+}
+
+
+
+
+
+
+function returnsANamedFunction() {
+    return function fuckMyLife() {console.log (`FUCK ME`);}
+}
+
+
+
+
+function returnsAnAnonymousFunction() {
+    return function() {console.log(`end this`)}
+}
